e66de59e2b40447ed33e06fa17a326ad54c8478d
styel07 <eleazarcorpuz@gmail.com>
2015-10-15 20:27:31 -1000

    created all the dependencies and set up the address and port

---
+++ server.js
@@ -0,0 +1,13 @@
+/** Use the net module to create a new server that listens on a specified
+*address 0.0.0.0 and port 6969 and listens for and accepts socket connections.
+*/
+
+var fs = require('fs');
+
+var net = require('net');
+
+var address = '0.0.0.0';
+
+var PORT = 6969;
+
+
