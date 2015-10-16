aa50c7f584ab81921efce588a69d5e5e286d5983
styel07 <eleazarcorpuz@gmail.com>
2015-10-16 11:44:24 -1000

    clients can now broadcast messages to each other

--- client.js
+++ client.js
@@ -1,16 +1,27 @@
 var net = require('net');
 
+var readableStandardInput =  process.stdin;
+var writableStandardOutput = process.stdout;
+
 var client = new net.Socket();
+
+// connects to the server on port 6969
 client.connect(6969, '0.0.0.0', function() {
-  console.log('Client Connected');
   client.write('Hello, server! Love, Client.');
 });
 
-client.on('data', function(data) {
-  console.log('Received: ' + data);
-  client.destroy(); // kill client after server's response
+// sends a message to the server
+readableStandardInput.on('data', function(chunk) {
+  client.write(chunk.toString());
 });
 
+// Receives a message from the server
+client.on('data', function(data) {
+  console.log('Received: ' + data);
+});
+
+// when the server stops connection
 client.on('close', function() {
   console.log('Connection closed');
 });
+
--- server.js
+++ server.js
@@ -10,17 +10,46 @@ var address = '0.0.0.0';
 
 var PORT = 6969;
 
+var clientList = [];
 
-var server = net.createServer(function(c) { //'connection' listener
+var server = net.createServer(function(client) { //'connection' listener
   console.log('client connected');
 
-  c.on('end', function() {
+  // Identify the client
+  client.name = client.remoteAddress + ':' + client.remotePort;
+
+  // adds the new client to a list
+  clientList.push(client);
+
+  // sends a nice welcome message and announce
+  client.write('Welcome' + client.name + '\n');
+  broadcast(client.name + 'joined the chat\n', client);
+
+  // handle incoming messages from clients
+  client.on('data', function(inputFromClient) {
+    broadcast(client.name + ' says: ' + inputFromClient, client);
+  });
+
+  // client disconnects
+  client.on('end', function() {
     console.log('client disconnected');
   });
 
-  c.write('hello\r\n');
-  c.pipe(c);
+  // sends the message to all connected clients
+  function broadcast(message, sender) {
+    clientList.forEach(function (client) {
+      // Dont send the message to the sender
+      if (client === sender) return;
+      client.write(message);
+    });
 
+    // Log it to the server output too
+    process.stdout.write(message);
+  }
+
+  // // client writes data to input
+  // client.write('hello\r\n');
+  // client.pipe(client);
 });
 
 server.listen({
